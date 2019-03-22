7a28abcea5fa860157bf03c4e2ed00a56776ef1c
Brandon_J_Allison <you@example.com>
2019-03-22 13:56:53 -0400

    final commit

--- starwars/src/components/Pages.js
+++ starwars/src/components/Pages.js
@@ -1,25 +1,5 @@
 import React from 'react'
-const btnStyle = {
-    margin: '20px',
-    bacgkround: 'white',
-    borderRadius: '12px',
-    height: '50px',
-    width: '100px'
-}
 
-
-const Pages = (props) => {
-    console.log('page', props)
-    return(
-<div>
-
-
-<button onClick={this.state.loadNextPage} style ={btnStyle}>Next</button>
-
-</div>
-
-    )
-}
 
 
 export default Pages;
